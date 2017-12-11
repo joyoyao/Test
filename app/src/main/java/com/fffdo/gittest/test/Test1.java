62bc55be057c49b1232932ce4475655f10548bb2
yaozaiyong <yaozaiyong@baidu.com>
2017-12-11 10:34:05 +0800

    test2

---
+++ app/src/main/java/com/fffdo/gittest/test/Test1.java
@@ -0,0 +1,8 @@
+package com.fffdo.gittest.test;
+
+/**
+ * Created by yaozaiyong on 2017/12/11.
+ */
+
+public class Test1 {
+}
---
+++ app/src/main/java/com/fffdo/gittest/test2/Test2.java
@@ -0,0 +1,8 @@
+package com.fffdo.gittest.test2;
+
+/**
+ * Created by yaozaiyong on 2017/12/11.
+ */
+
+public class Test2 {
+}
