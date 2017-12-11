3c641db34abd2cdb521bbb631e56c96fd9f0fefb
yaozaiyong <yaozaiyong@baidu.com>
2017-12-11 10:52:55 +0800

    adf

--- app/src/main/java/com/fffdo/gittest/test/Test3.java
+++ app/src/main/java/com/fffdo/gittest/test/Test3.java
@@ -1,8 +1,12 @@
 package com.fffdo.gittest.test;
 
+import com.fffdo.gittest.test2.Test1;
+
 /**
  * Created by yaozaiyong on 2017/12/11.
  */
 
 public class Test3 {
+    private String tes2;
+    private Test1 test1;
 }
