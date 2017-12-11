4235af9222f477eae57a4ec5619c22c5205a6f3e
yaozaiyong <yaozaiyong@baidu.com>
2017-12-11 10:50:52 +0800

    test3

--- app/src/main/java/com/fffdo/gittest/test/Test3.java
+++ app/src/main/java/com/fffdo/gittest/test/Test3.java
@@ -4,9 +4,5 @@ package com.fffdo.gittest.test;
  * Created by yaozaiyong on 2017/12/11.
  */
 
-public class Test1 {
-
-    private  String t2;
-
-    private  String t3;
+public class Test3 {
 }
---
+++ app/src/main/java/com/fffdo/gittest/test2/Test1.java
@@ -0,0 +1,16 @@
+package com.fffdo.gittest.test2;
+
+import com.fffdo.gittest.test.Test3;
+
+/**
+ * Created by yaozaiyong on 2017/12/11.
+ */
+
+public class Test1 {
+
+    private  String t2;
+
+    private  String t3;
+
+    private Test3 test3;
+}
